67a969b041ba424221abdf5f039d0c472d96eae3
Mitchell Powell <mdp9648@uncw.edu>
2015-11-03 18:44:39 -0500

    Updated default close operation of GameWindow

--- src/GameWindow.java
+++ src/GameWindow.java
@@ -5,6 +5,7 @@ public class GameWindow extends JFrame{
 	
 	public GameWindow(){
 		setTitle("Tetris 2: Son of Tetris");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setSize(1000, 750);
 		this.setResizable(false);
 		this.setVisible(true);
