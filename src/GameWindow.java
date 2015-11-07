821b75e7f9164461c7e4b0f7b179ae12ec0cffa1
Mitchell Powell <mdp9648@uncw.edu>
2015-11-07 17:20:03 -0500

    Added background image to main game window

--- src/GameWindow.java
+++ src/GameWindow.java
@@ -1,13 +1,31 @@
+import java.awt.Color;
+import java.awt.Image;
+import java.awt.Toolkit;
+
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 
 public class GameWindow extends JFrame{
 	
+	private ImageIcon backgroundIMG;
+	private JLabel background;
 	public GameWindow(){
+		
+		backgroundIMG = new ImageIcon(Toolkit.getDefaultToolkit().createImage("Tetris_Background.jpg"));
+		background = new JLabel();
+		background.setIcon(backgroundIMG);
 		setTitle("Tetris 2: Son of Tetris");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		this.setSize(1000, 750);
+		
+		this.setSize(900, 700);
 		this.setResizable(false);
+		//setBackground(Color.LIGHT_GRAY);
+		PlayScreen screen = new PlayScreen(this);
+		add(screen);
+		add(background);
+		
 		this.setVisible(true);
 	}
 
---
+++ src/PlayScreen.java
@@ -0,0 +1,14 @@
+import java.awt.Color;
+
+import javax.swing.JPanel;
+
+
+public class PlayScreen extends JPanel{
+	
+	public PlayScreen(GameWindow w){
+		//setSize(w.getWidth()/2,w.getHeight()-40);
+		setSize(300,200);
+		setVisible(true);
+		
+	}
+}
