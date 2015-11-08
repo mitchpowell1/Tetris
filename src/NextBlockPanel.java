6f3bd9d6241751446e476bc586cc66dda4ec56cd
Mitchell Powell <mdp9648@uncw.edu>
2015-11-08 14:39:33 -0500

    Edited NextBlockPanel text

--- src/NextBlockPanel.java
+++ src/NextBlockPanel.java
@@ -9,7 +9,7 @@ public class NextBlockPanel extends BlockDisplayPanel {
 	public NextBlockPanel(){
 		setLocation(950,50);
 		setSize(200,200);
-		TitledBorder title = BorderFactory.createTitledBorder("Next Block");
+		TitledBorder title = BorderFactory.createTitledBorder("Next");
 		title.setTitleColor(Color.LIGHT_GRAY);
 		title.setBorder(BorderFactory.createEtchedBorder(Color.LIGHT_GRAY,Color.DARK_GRAY));
 		title.setTitleJustification(2);
